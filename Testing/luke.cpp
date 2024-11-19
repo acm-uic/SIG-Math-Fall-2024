b58817ef7afa12c6da4abfc30c48b52e32a7d1f1
ld-uic <ldean7@uic.edu>
2024-11-19 12:23:25 -0600

    add makefile to test each individual test file

--- Testing/luke.cpp
+++ Testing/luke.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
+#include <string>
+
+// BAD PRACTICE, JUST FOR TESTING
+#include "../Parser/InputParser.h"
+
+using namespace std;
 
 int main()
 {
     std::cout << "Hello, world!\n";
+
+    string test;
+
+    cout << "Input a string: ";
+    cin >> test;
+
+    cout << endl;
+
+    cout << "Your string with parenthesis: ";
+
+    InputParser myParse = InputParser();
+
+    cout << myParse.findMatchingParenthesis(test) << endl;
+    
+    
     return 0;
 }
---
+++ makefile
@@ -0,0 +1,3 @@
+# Compile using Luke's Testing file
+luke:
+	g++ -Wall Testing/luke.cpp Parser/Expression.cpp Parser/InputParser.cpp -o luke.out
